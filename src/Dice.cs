e45de0182008098f8b39319774833f6896c3e110
samuelbigos <bigos.sam+github@googlemail.com>
2022-07-17 13:23:35 +0100

    Refactor face meshes to allow for one-ups.

--- src/Dice.cs
+++ src/Dice.cs
@@ -12,9 +12,15 @@ public partial class Dice : GridObject
     [Export] private Vector2 _start = new Vector2(5, 5);
     [Export] private NodePath _indicatorPath;
     
+    [Export] private List<Mesh> _numberMeshes;
+    [Export] private List<Mesh> _bulletMeshes;
+    [Export] private List<Mesh> _healMeshes;
+    [Export] private List<Mesh> _lightningMeshes;
+    [Export] private List<Mesh> _coinMeshes;
+    
     [OnReadyGet] private Game _game;
     [OnReadyGet] private Camera _camera;
-
+    
     public float MoveTime => _moveTime;
     public float MaxHealth => _maxHealth;
     public float Health => _health;
@@ -36,9 +42,10 @@ public partial class Dice : GridObject
     private int _backFace;
 
     private int[] _faceValues = new[] {1, 2, 3, 4, 5, 6};
-
+    
     private Dictionary<int, ModDice> _mods = new();
-    private Dictionary<ModManager.ModTypes, List<MeshInstance>> _faces = new();
+    private Dictionary<ModManager.ModTypes, List<Mesh>> _meshes = new();
+    private List<MeshInstance> _faces = new List<MeshInstance>();
     
     protected override void Ready()
     {
@@ -58,30 +65,49 @@ public partial class Dice : GridObject
         _mods[5] = new ModDice(ModManager.ModTypes.Number);
         
         _instance = this;
-
-        for (int i = 0; i < 6; i++)
+        
+        for (int i = 0; i < (int)ModManager.ModTypes.COUNT; i++)
         {
-            for (int j = 0; j < (int)ModManager.ModTypes.COUNT; j++)
+            switch ((ModManager.ModTypes) i)
             {
-                ModManager.ModTypes type = (ModManager.ModTypes) j;
-                if (!_faces.ContainsKey(type))
-                    _faces[type] = new List<MeshInstance>();
-                
-                List<MeshInstance> list = _faces[type];
-                MeshInstance mesh = GetNode<MeshInstance>($"{type.ToString()}{i + 1}");
-                list.Add(mesh);
-                ShaderMaterial mat = mesh.MaterialOverride as ShaderMaterial;
-                mat = mat.Duplicate() as ShaderMaterial; // set unique material so we can change colour
-                mesh.MaterialOverride = mat;
-                mat.SetShaderParam("u_col_1", ModManager.Instance.ModColours[type]);
-                mat.SetShaderParam("u_col_2", ModManager.Instance.ModColoursSecondary[type]);
+                case ModManager.ModTypes.Number:
+                    _meshes[(ModManager.ModTypes) i] = _numberMeshes;
+                    break;
+                case ModManager.ModTypes.Bullet:
+                    _meshes[(ModManager.ModTypes) i] = _bulletMeshes;
+                    break;
+                case ModManager.ModTypes.Heal:
+                    _meshes[(ModManager.ModTypes) i] = _healMeshes;
+                    break;
+                case ModManager.ModTypes.Lightning:
+                    _meshes[(ModManager.ModTypes) i] = _lightningMeshes;
+                    break;
+                case ModManager.ModTypes.Coin:
+                    _meshes[(ModManager.ModTypes) i] = _coinMeshes;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
         for (int i = 0; i < 6; i++)
         {
-            _faces[ModManager.ModTypes.Number][i].Visible = true;
+            _faces.Add(GetNode<MeshInstance>($"{i}"));
+            
+            ShaderMaterial mat = _faces[i].MaterialOverride as ShaderMaterial;
+            mat = mat.Duplicate() as ShaderMaterial; // set unique material so we can change colour
+            _faces[i].MaterialOverride = mat;
+            SetFace(i, ModManager.ModTypes.Number, _faceValues[i]);
         }
+    }
+
+    private void SetFace(int face, ModManager.ModTypes type, int value)
+    {
+        ShaderMaterial mat = _faces[face].MaterialOverride as ShaderMaterial;
+        mat.SetShaderParam("u_col_1", ModManager.Instance.ModColours[type]);
+        mat.SetShaderParam("u_col_2", ModManager.Instance.ModColoursSecondary[type]);
+
+        _faces[face].Mesh = _meshes[type][value - 1];
     }
 
     public override void _Process(float delta)
@@ -211,13 +237,10 @@ public partial class Dice : GridObject
     public void OnPickup(ModPickup pickup)
     {
         int face = _bottomFace;
-        SetFaceVisible(face, false);
-        
         ModDice mod = new ModDice(pickup.Type);
         mod.Activate(_gridPos, _forward, _faceValues[_bottomFace]);
         _mods[face] = mod;
-        
-        SetFaceVisible(face, true);
+        SetFace(face, pickup.Type, _faceValues[face]);
     }
     
     public bool OnOneUp(OneUp pickup)
@@ -226,17 +249,11 @@ public partial class Dice : GridObject
         if (_faceValues[face] == 6)
             return false;
         
-        SetFaceVisible(face, false);
         _faceValues[face] += 1;
-        SetFaceVisible(face, true);
-        
+        SetFace(face, _mods[face].Type, _faceValues[face]);
         UpdateIndicator();
+        
         return true;
-    }
-
-    private void SetFaceVisible(int face, bool visible)
-    {
-        _faces[_mods[face].Type][_faceValues[face] - 1].Visible = visible;
     }
     
     private Vector3 FaceDirection(int face, Basis basis)
